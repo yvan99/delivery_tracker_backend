f8d267cd4629625601a10e147ca33994d9e253ea
yvan99 <ishimweyvan90@gmail.com>
2024-04-26 15:43:58 +0200

    ⚡️ finish up with the basic backend endpoints

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const connectDB = require('./src/api/config/db');
-
+const connectDB = require("./src/api/config/db");
+const bodyParser = require("body-parser");
 const express = require("express");
+
+const locationRoutes = require("./src/api/routes/locationRoutes");
+const routeRoutes = require("./src/api/routes/routeRoutes");
+const vehicleRoutes = require("./src/api/routes/vehicleRoutes");
+const deliveryRoutes = require("./src/api/routes/deliveryRoutes");
+
 const app = express();
 connectDB();
 
+app.use(express.json())
+// ROUTES
+app.use("/api/locations", locationRoutes);
+app.use("/api/routes", routeRoutes);
+app.use("/api/vehicles", vehicleRoutes);
+app.use("/api/deliveries", deliveryRoutes);
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
-}); 
+});
--- src/api/config/db.js
+++ src/api/config/db.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGO_URI)
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
